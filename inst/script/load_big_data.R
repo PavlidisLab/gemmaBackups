971d84cf463b5a8aac2eb73772924e941c070099
OganM <ogan.mancarcii@gmail.com>
2024-02-21 22:24:56 -0800

    documentation fix

--- inst/script/load_big_data.R
+++ inst/script/load_big_data.R
@@ -4,7 +4,7 @@
 #' automatically loaded during package load. Use this
 #' function to load them into an environment
 #'
-#' @param load Which objects to load
+#' @param load Which objects to load. Defaults to all available objects
 #' @param envir Which environments to load the objects into
 #' @export
 load_big_data = function(load = <capture.output(dput(available))>,
